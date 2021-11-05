fc202c59a459e9f204028203dff21e4c0f786820
Igor Nalepka <nalepkaigor@gmail.com>
2021-11-05 09:34:00 +0100

    Task-20: info about login person

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -16,9 +16,9 @@ export class HeaderComponent implements OnInit {
   isAuth: boolean = false;
 
   constructor(public usersService: UsersService,
-              private router: Router,
-              private toastr: ToastrService,
-              private auth: AuthService) {}
+    private router: Router,
+    private toastr: ToastrService,
+    private auth: AuthService) { }
 
   ngOnInit(): void {
     this.auth.isLoggedIn.subscribe(data => {
@@ -26,36 +26,30 @@ export class HeaderComponent implements OnInit {
       if (this.isAuth) {
         this.isAuth = true;
         let id: number = Number(localStorage.getItem('id'))
-      this.getUserInfo(id);
+        this.getUserInfo(id);
       }
-    });  
+    });
   }
 
   getUserInfo(id: number): void {
     let userId = id
     this.usersService.getUser(userId).subscribe(
       (user) => {
-        this.userName = JSON.stringify(user.data.name).substr(1,JSON.stringify(user.data.name).length-2);
-        this.userSurname = JSON.stringify(user.data.surname).substr(1,JSON.stringify(user.data.surname).length-2);
+        this.userName = user.data.name;
+        this.userSurname = user.data.surname;
       }, err => {
         console.log('err' + err);
-      }) 
-  } 
+      })
+  }
 
-  logout() {
-/*
-    localStorage.removeItem('token');
+  logout(): void {
+    this.auth.logout();
     this.usersService.userSubject$.next(null);
     this.showToasterLogoutUser();
-    this.router.navigate(['/login']); */
-  
-  this.auth.logout();
-  this.usersService.userSubject$.next(null);
-  this.showToasterLogoutUser();
-  this.router.navigate(['/login']);
-} 
+    this.router.navigate(['/login']);
+  }
 
-  showToasterLogoutUser() : void {
+  showToasterLogoutUser(): void {
     this.toastr.success("Wylogowano!");
   }
 }
